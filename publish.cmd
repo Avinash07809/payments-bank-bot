6c9d368a96ebecc51f7f445c402dcdb30995adef
Avinash07809 <91564791+Avinash07809@users.noreply.github.com>
2022-03-06 11:29:03 +0530

    Add files via upload

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=.\payments-bank-bot-a85f-Web-Deploy.pubxml -p:Password=jExrPoeZgfMvRPGtRqmRG9DdeLWfPh8KcTXeuq3sn6qmtk2SSJvzNxtEiCh9
+
